ffcf0fa221b07902c00ce6252677a0502976100c
allankimanzi2 <allankim457@gmail.com>
2024-02-28 00:56:26 +0300

    Done assignment

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+# lib/app.py
+print("Hello world!")
+print("Hello moon!")
+print("Hello mars!")
+
+print("Hello Allan!", end=" ")
+print("Hello moon!", end="_")
+print("Hello mars!", end="!!\n")
+
+print("Hello World!", end=" ")
+print("Pass this test,", end=" ")
+print("please.", end="\n")
